b3556a696af407190f3022ca1a7d9a32e7001348
loyal <102851403+loyaltly-cn@users.noreply.github.com>
2025-07-16 14:56:53 +0800

    Update OSS.java

--- springboot/src/main/java/com/spingas/account/springboot/oss/OSS.java
+++ springboot/src/main/java/com/spingas/account/springboot/oss/OSS.java
@@ -5,10 +5,10 @@ import com.aliyun.oss.OSSClientBuilder;
 
 public class OSS {
     public static void main(String[] args) {
-        String endpoint = "https://spingas.oss-ap-southeast-1.aliyuncs.com";
+        String endpoint = "xxxxxxxxxxxxxx";
 // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = "LTAI5tH9FiZkhmY8ku46TwtT";
-        String accessKeySecret = "jfue556zrsJxnGMywhkbVRFMMwb01e";
+        String accessKeyId = "xxxxxxxxxxxxxxxx";
+        String accessKeySecret = "xxxxxxxxxxxxxxxxx";
 
 // 创建ClientConfiguration实例，您可以根据实际情况修改默认参数。
         ClientBuilderConfiguration conf = new ClientBuilderConfiguration();
